cd5c9bf992059ccccb4f1796649fab7fed5e680b
jj-attaq <jj.attaq@gmail.com>
2025-08-01 09:34:53 +0200

    repl implemented with bufio

--- repl.go
+++ repl.go
@@ -1,10 +1,28 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
 
-func cleanInput(text string) []string {
+func StartRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Printf("Pokedex > ")
+		scanner.Scan()
+		words := CleanInput(scanner.Text())
+		if len(words) == 0 {
+			continue
+		}
+
+		commandName := words[0]
+		fmt.Printf("Your command was: %s\n", commandName)
+	}
+}
+
+func CleanInput(text string) []string {
 	var result []string
 	output := strings.ToLower(text)
 	result = strings.Fields(output)
